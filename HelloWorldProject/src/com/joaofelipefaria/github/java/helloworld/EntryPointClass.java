9c57b3560aed3f58995a9fc3c45dad4e4c50f026
joaofelipefaria <joao.assencao@mrv.com.br>
2018-05-19 11:57:56 -0300

    projeto hava hello world

---
+++ HelloWorldProject/src/com/joaofelipefaria/github/java/helloworld/EntryPointClass.java
@@ -0,0 +1,10 @@
+package com.joaofelipefaria.github.java.helloworld;
+
+public class EntryPointClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is J. Felipe Faria");
+		System.out.println("Saying: HELLO WORLD!");
+	}
+
+}
